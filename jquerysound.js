2a95d1c8e69e065c9eb59487f56b2c49cb08c65c
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 05:47:48 -0700

    fix sound player again

--- jquerysound.js
+++ jquerysound.js
@@ -1,16 +1,9 @@
-/**
- * @author Alexander Manzyuk <admsev@gmail.com>
- * Copyright (c) 2012 Alexander Manzyuk - released under MIT License
- * https://github.com/admsev/jquery-play-sound
- * Usage: $.playSound('http://example.org/sound.mp3');
-**/
-
-(function($){
-
-  $.extend({
-    playSound: function(){
-      return $("<embed src='"+arguments[0]+".mp3' hidden='true' autostart='true' loop='false' class='playSound'>" + "<audio autoplay='autoplay' style='display:none;' controls='controls'><source src='"+arguments[0]+".mp3' /><source src='"+arguments[0]+".ogg' /></audio>").appendTo('body');
-    }
-  });
-
-})(jQuery);
+function playsound(soundobj) {
+  var embed = document.createElement('embed');
+  embed.setAttribute('width',1);
+  embed.setAttribute('height',1);
+  embed.setAttribute('src',soundobj);
+  embed.setAttribute('autostart', false);
+  embed.setAttribute('enablejavascript', true);
+  embed.Play();
+}
