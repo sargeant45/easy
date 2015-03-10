ec0f6bcc2d6901e92f8b26b46ee9a831aaca186c
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 05:52:49 -0700

    remove old sound player

--- jquerysound.js
+++
@@ -1,9 +0,0 @@
-function playsound(soundobj) {
-  var embed = document.createElement('embed');
-  embed.setAttribute('width',1);
-  embed.setAttribute('height',1);
-  embed.setAttribute('src',soundobj);
-  embed.setAttribute('autostart', false);
-  embed.setAttribute('enablejavascript', true);
-  embed.Play();
-}
